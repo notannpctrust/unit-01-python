2f4a23519f174ab8ee986fd9802f156efcb31f61
Christopher Brown <cbrown2@brooklynsteamcenter.org>
2025-09-12 09:43:05 -0400

    Im almost done pleeeeeeease

--- 01_data_types.py
+++ 01_data_types.py
@@ -3,7 +3,9 @@ TASK 1:
 Create a float variable, and then convert it to an integer
 """
 
-
+print("------task1---------")
+print()
+print()
 
 # Making a variable 
 my_float = 3.1459
@@ -21,6 +23,12 @@ TASK 2:
 Write code that takes a number as input and prints whether 
 it's positive, negative, or zero using if-elif-else statements.
 """
+
+print("------task2---------")
+print()
+print()
+
+
 # Input a random number and concert it to a number
 number = float(input("type a number"))
 
@@ -31,6 +39,62 @@ elif number < 0:
     print("The number is negative.")
 else:
     print("The number is zero.")
+
+
+"""
+TASK 3 
+Write code that takes two numbers as input (an integer and a float), 
+performs addition, subtraction, multiplication, and division, and prints the results.
+
+"""
+
+print("------task3---------")
+print()
+print()
+
+#Take integer input from the user
+input1 = input("give me a float")
+
+# Take float input from the user
+num1= float(input1)
+
+#Printing the results
+print(type(num1))
+
+"""
+TASK4 
+
+Create a dictionary with keys as fruit names and values as their respective quantities. 
+Then print out the quantity of one of the fruits.
+"""
+
+print("------task4---------")
+print()
+print()
+
+#Creating the dictionary
+fruits = {
+   'apple':18,
+   'watermelon':30,
+   'orange':17,
+
+ }
+
+#Printing the results
+print("18:", fruits["apple"])
+
+
+"""
+TASK5
+Create a string variable with that is a list of numbers and convert that string into a tuple.
+Then print out the both the original string and tuple.
+"""
+
+print("------task5---------")
+print()
+print()
+
+
 
 
 
