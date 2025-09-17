0381c3e554230c6de8b48b3b4626cf17f0e1ef26
Christopher Brown <cbrown2@brooklynsteamcenter.org>
2025-09-17 10:54:01 -0400

    I am that smart

--- 04_condtional_calisthenics.py
+++ 04_condtional_calisthenics.py
@@ -28,10 +28,12 @@ print("------task2---------")
 print()
 print()
 
-
-age = 179
+#Setting up a random age,the age may seem large but it's needed for the if statement
+age = 19
+#Getting an input statement for the ticket price 
 student = input("Are you a student? ")
 
+#The if and else work for asking the different ticket prices for students/age.
 if age <= 18 or student == "yes":
     print("Your ticket price will be $10")
 else:
@@ -49,6 +51,11 @@ print("------task3---------")
 print()
 print()
 
+myList = ["apple", "banana", "blueberries"]
 
+fruit = input ("What fruits are in the list?")
 
-
+if fruit in myList:
+   print("That fruit is on the list")
+else:
+    print("That fruit is not on the list")
