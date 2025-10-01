f957d2fcf20fb4bab614192bdecd1f0313f629f6
Christopher Brown <cbrown2@brooklynsteamcenter.org>
2025-10-01 10:53:24 -0400

    yippe

--- project_01_todo.py
+++ project_01_todo.py
@@ -1,4 +1,6 @@
-to_do_list = []#Making a list named to do list
+with open("to_do_list.txt", "r") as file:
+    to_do_list = file.readlines()
+
 
 while True:#Using a True statement for my while loop to let me keep the loop go on forever
     print()
@@ -16,6 +18,7 @@ while True:#Using a True statement for my while loop to let me keep the loop go 
     print()
     print("Option3:edit")
     print()
+    print("Option4:Clear all")
     choice = input("What would you like to do?:")
     print()
     if choice == 'add':
@@ -25,7 +28,14 @@ while True:#Using a True statement for my while loop to let me keep the loop go 
     elif choice == 'delete':
         deleted_item = int(input("Enter your # deleted item(dont type the # just the number):"))#Using # for numbered deletes
         del to_do_list[deleted_item -1]#Using the same layout as the "add" choice for simplicity
+
+    elif choice == "exit":
+        with open("to_do_list.txt", "w") as file:
+            file.write("\n".)
+     
         
+
+    
     
     
 
